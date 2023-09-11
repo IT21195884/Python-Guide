4212fd2052f331938e4cf6518d6091ded90b3f86
IT21195884 <99194695+IT21195884@users.noreply.github.com>
2023-09-12 01:37:15 +0530

    Second Commit

--- test.py
+++ test.py
@@ -7,3 +7,32 @@ x = 4
 y = "Sally"
 a=b=c=1
 e,f,g = 1,2.00,"John"
+
+print(x)
+
+print(g)
+
+print(type(x))
+
+print(f)
+
+#Mathematics Operators
+
+    #Float Division    - /
+
+k = 50/20
+
+
+    #Integer Division  - //
+
+l = 50//20
+
+    #Power             - **
+
+m = 5**2
+
+print(k)
+print(l)
+print(m)
+
+#   +=     ->     i=i+2
