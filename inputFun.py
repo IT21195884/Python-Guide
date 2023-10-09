a300accfcea624ebd1ee42d4b620b215f8fe7297
IT21195884 <99194695+IT21195884@users.noreply.github.com>
2023-10-09 14:46:51 +0530

    9th October 2023

--- inputFun.py
+++ inputFun.py
@@ -1,10 +1,10 @@
 #input() function      *works as a prompt
 
-a=input('Enter a number :')
+a=input('Enter a number here :')
 print(a)
 
-a=int(input('Enter a number :'))
+a=int(input('Enter a number here:')
 print(a)
 
-a=float(input('Enter a floating number :'))
+a=float(input('Enter a floating number here :'))
 print(a)
